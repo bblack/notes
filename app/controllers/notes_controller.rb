6167197e9146e9a5ee8183ffa92179c04294db2b
Brian Black <black@gatech.edu>
2016-08-07 16:13:25 -0400

    show note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,8 @@ class NotesController < ApplicationController
     def index
         render locals: {notes: Note.all}
     end
+
+    def show
+        render locals: {note: Note.find(params[:id])}
+    end
 end
