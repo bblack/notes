2440166952b19eb60765e66125744175faaa3688
Brian Black <black@gatech.edu>
2016-08-07 16:32:38 -0400

    create note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,19 @@ class NotesController < ApplicationController
     def show
         render locals: {note: Note.find(params[:id])}
     end
+
+    def new
+        render locals: {note: Note.new}
+    end
+
+    def create
+        note = Note.create(note_params)
+        redirect_to(note_path(note))
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :body)
+    end
 end
